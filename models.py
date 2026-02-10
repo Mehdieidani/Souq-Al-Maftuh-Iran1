aba5f7df5501b99d07f10d1f186eed877fc463b3
Mehdieidani <161817237+Mehdieidani@users.noreply.github.com>
2026-02-10 22:52:53 +0330

    Add Message model for user communication

--- models.py
+++ models.py
@@ -1,14 +1,5 @@
-from django.db import models
-from django.contrib.auth.models import User
-
-class Ad(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
-    description = models.TextField()
-    price = models.CharField(max_length=50, blank=True)
-    image = models.URLField(blank=True)
-    is_approved = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.title
+class Message(models.Model):
+    sender = models.ForeignKey(User, on_delete=models.CASCADE)
+    ad = models.ForeignKey(Ad, on_delete=models.CASCADE)
+    text = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
